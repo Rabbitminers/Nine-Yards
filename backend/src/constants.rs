34c9f819777da356f2a69a7d711180ed99cf9e35
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-04 13:12:47 +0100

    Added user constants

--- backend/src/constants.rs
+++ backend/src/constants.rs
@@ -1,5 +1,9 @@
 pub const IGNORE_ROUTES: [&str; 3] = ["/api/ping", "/api/auth/signup", "/api/auth/login"];
 pub const AUTHORIZATION: &str = "Authorization";
 pub const EMPTY: &str = "";
+
 pub const MESSAGE_INVALID_TOKEN: &str = "Invalid token, please login again";
 pub const MESSAGE_INTERNAL_SERVER_ERROR: &str = "Internal Server Error";
+
+pub const ERROR_USERNAME: &str = "User name is already used";
+pub const MESSAGE_SIGNUP_SUCCESS: &str = "Signup successfully";
