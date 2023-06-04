8acb7eedceb2e3b99b6721bd6ce0bc7b8808914f
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-04 13:12:26 +0100

    Added init migrations

---
+++ backend/migrations/20230603155502_init.down.sql
@@ -0,0 +1,12 @@
+-- Add down migration script here
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS teams;
+
+DROP TABLE IF EXISTS team_members;
+
+DROP TABLE IF EXISTS projects;
+
+DROP TABLE IF EXISTS task_groups;
+
+DROP TABLE IF EXISTS tasks;
---
+++ backend/migrations/20230603155502_init.up.sql
@@ -0,0 +1,47 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS users (
+    id TEXT PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+	password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    icon_url TEXT,
+	login_session TEXT
+);
+
+CREATE TABLE IF NOT EXISTS teams (
+    id TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS team_members (
+    id TEXT PRIMARY KEY NOT NULL,
+    team_id TEXT REFERENCES teams NOT NULL,
+    user_id TEXT REFERENCES users NOT NULL,
+    permissions INTEGER NOT NULL,
+    accepted BOOLEAN NOT NULL,
+    member_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    id TEXT PRIMARY KEY NOT NULL,
+    team_id TEXT REFERENCES teams NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task_groups (
+    id TEXT PRIMARY KEY NOT NULL,
+    project_id TEXT REFERENCES projects NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id TEXT PRIMARY KEY NOT NULL,
+    project_id TEXT REFERENCES projects NOT NULL,
+    task_group_id TEXT REFERENCES task_groups NOT NULL,
+    title TEXT NOT NULL,
+    summary TEXT,
+    creator TEXT REFERENCES users NOT NULL,
+    assignee TEXT REFERENCES users NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS notifications (
+    id TEXT PRIMARY KEY NOT NULL,
+    user_id TEXT REFERENCES users NOT NULL
+);
