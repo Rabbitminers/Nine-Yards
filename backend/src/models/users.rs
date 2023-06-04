7cdef03992f0473cfe85a91d8b4fb66666d426aa
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-04 13:13:23 +0100

    Added registering to users

--- backend/src/models/users.rs
+++ backend/src/models/users.rs
@@ -1,171 +1,107 @@
-use sqlx::{SqlitePool, FromRow};
+use bcrypt::{DEFAULT_COST, hash};
+use sqlx::{SqlitePool, error::DatabaseError};
 use uuid::Uuid;
+use crate::{constants, models::ids::generate_user_id};
 
-use super::ids::{Base62Id};
-use super::user_token::UserToken;
+use super::ids::UserId;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(from = "Base62Id")]
-#[serde(into = "Base62Id")]
-pub struct UserId(pub u64);
-
-pub const DELETED_USER: UserId = UserId(710324008456789);
-
-#[derive(FromRow, Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: UserId,
     pub username: String,
     pub password: String,
     pub email: String,
-    pub icon_url: String,
-    pub login_session: String,
+    pub login_session: Option<String>
 }
 
-pub struct UserDTO {
+pub struct Register {
     pub username: String,
-    pub email: String,
     pub password: String,
-    pub icon_url: String
+    pub email: String
 }
-
-#[derive(Serialize, Deserialize)]
-pub struct LoginDTO {
-    pub username_or_email: String,
-    pub password: String,
-}
-
-pub struct LoginInfoDTO {
-    pub username: String,
-    pub login_session: String,
-}
-
 
 impl User {
-    pub async fn signup(user: UserDTO, conn: &SqlitePool) -> Result<(), sqlx::Error> {
-        let passh = user.passhash();
-        let id = generate_user_id();
+    pub async fn register(
+        data: Register, 
+        conn: &SqlitePool
+    ) -> Result<String, String> {
+        match Self::find_by_username(&data.username, conn).await {
+            Ok(None) => {
+                let hashed_pwd = hash(&data.password, DEFAULT_COST).unwrap();
+                let user_id = generate_user_id(conn).await;
+                
+                if let Ok(ido) = user_id {
+                    User {
+                        id: ido,
+                        username: data.username,
+                        password: hashed_pwd,
+                        email: data.email,
+                        login_session: None
+                    }
+                    .insert(conn);
 
+                    Ok(constants::MESSAGE_SIGNUP_SUCCESS.to_string())
+                } else {
+                    Err(constants::MESSAGE_INTERNAL_SERVER_ERROR.to_string())
+                }
+            }
+            _ => Err(format!("User '{}' is already registered", &data.username))
+        }
+    }
+
+    pub async fn insert(
+        &self,
+        conn: &SqlitePool
+    ) -> Result<(), sqlx::error::Error> {
         sqlx::query!(
-            r#"INSERT INTO users (
-                id, username, password, email, icon_url
+            "
+            INSERT INTO users (
+                id, username, password, email, login_session
             )
             VALUES (
-                $1 ,$2 ,$3, $4, $5
-            )"#,
-            id as UserId, 
-            user.username, 
-            passh, 
-            user.email, 
-            "default"
+                $1, $2, $3, $4, $5
+            )
+            ",
+            self.id.0,
+            self.username,
+            self.password,
+            self.email,
+            self.login_session
         )
         .execute(conn)
-        .await
-        .map(|d| d.rows_affected());
+        .await?;
 
         Ok(())
     }
 
-    pub async fn logout(user_id: UserId, conn: &SqlitePool) -> Result<(), sqlx::Error> {
-        unimplemented!()
-    }
-
-    pub async fn is_valid_login_session(user_token: &UserToken, conn: &SqlitePool) -> bool {
-        let query = sqlx::query!(
-            "
-            SELECT * FROM users
-            WHERE username = ? 
-            AND login_session = ?
-            ",
-            user_token.username,
-            user_token.login_session,
-        );
-    
-        if let Ok(user) = query.fetch_optional(conn).await {
-            user.is_some()
-        } else {
-            false
-        }
-    }
-
-    pub async fn find_user_by_username(un: &str, conn: &SqlitePool) -> Result<Option<Self>, sqlx::error::Error> {
+    pub async fn find_by_username(
+        username: &String, 
+        conn: &SqlitePool
+    ) -> Result<Option<Self>, sqlx::error::Error> {
         let result = sqlx::query!(
             "
-            SELECT * 
-            FROM users 
-            WHERE LOWER(username) = LOWER(?)
+            SELECT id, username, password, email, login_session
+            FROM users
+            WHERE username = $1
             ",
-            un
+            username
         )
         .fetch_optional(conn)
         .await?;
 
         if let Some(row) = result {
-            Ok(Some(User {
-                id: UserId(row.id.unwrap()),
-                username: row.username.unwrap(),
-                password: row.password.unwrap(),
-                email: row.email.unwrap(),
-                icon_url: row.icon_url.unwrap(),
-                login_session: row.login_session.unwrap()
+            Ok(Some(Self {
+                id: UserId(row.id),
+                username: row.username,
+                password: row.password,
+                email: row.email,
+                login_session: row.login_session
             }))
         } else {
             Ok(None)
         }
     }
 
-    pub async fn update_login_session_to_db(
-        un: &str,
-        login_session_str: &str,
-        conn: &SqlitePool,
-    ) -> bool {
-
-        if let Ok(Some(user)) = User::find_user_by_username(un, conn).await {
-            let result = sqlx::query!(
-                "UPDATE users SET login_session = ? WHERE id = ?",
-                login_session_str,
-                user.id
-            )
-            .execute(conn)
-            .await;
-
-            result.is_ok()
-        } else {
-            false
-        }
-    }
-
     pub fn generate_login_session() -> String {
         Uuid::new_v4().to_simple().to_string()
     }
-}
-
-impl UserDTO {
-    pub fn passhash(&self) -> String {
-        passhash(&self.username, &self.password)
-    }
-}
-
-use crate::database::models::user_item::User as DBUser;
-impl From<DBUser> for User {
-    fn from(data: DBUser) -> Self {
-        Self {
-            id: data.id.into(),
-            username: data.username,
-            password: data.password,
-            email: data.email,
-            icon_url: data.icon_url,
-            login_session: data.login_session,
-        }
-    }
-}
-
-fn passhash(name: &str, pass: &str) -> String {
-    let namedpass = format!("{}{}", name, pass);
-    let hash = bcrypt::hash(namedpass.as_bytes(), bcrypt::DEFAULT_COST).unwrap();
-    hash
-}
-
-fn passhash_verify(name: &str, pass: &str, hash: &str) -> bool {
-    let namedpass = format!("{}{}", name, pass);
-    bcrypt::verify(namedpass.as_bytes(), hash).unwrap()
 }
