bd4f805d4a42a90b197571636bcf2f9d68d35e94
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-05 23:18:39 +0100

    Updated task tables

--- backend/migrations/20230603155502_init.up.sql
+++ backend/migrations/20230603155502_init.up.sql
@@ -24,23 +24,26 @@ CREATE TABLE IF NOT EXISTS team_members (
 CREATE TABLE IF NOT EXISTS projects (
     id TEXT PRIMARY KEY NOT NULL,
     team_id TEXT REFERENCES teams NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     icon_url TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS task_groups (
     id TEXT PRIMARY KEY NOT NULL,
-    project_id TEXT REFERENCES projects NOT NULL
+    project_id TEXT REFERENCES projects NOT NULL,
+    name TEXT NOT NULL,
+    position INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
     id TEXT PRIMARY KEY NOT NULL,
     project_id TEXT REFERENCES projects NOT NULL,
     task_group_id TEXT REFERENCES task_groups NOT NULL,
-    title TEXT NOT NULL,
-    summary TEXT,
+    name TEXT NOT NULL,
+    information TEXT,
     creator TEXT REFERENCES users NOT NULL,
-    assignee TEXT REFERENCES users NOT NULL
+    assignee TEXT REFERENCES users NOT NULL,
+    position INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS notifications (
