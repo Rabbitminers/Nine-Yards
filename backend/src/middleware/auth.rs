0e485a28f716267f7c0b7dc6b5136b2a70fd9c9e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-09 17:57:28 +0100

    Implemented auth middleware

--- backend/src/middleware/auth.rs
+++ backend/src/middleware/auth.rs
@@ -1,43 +1,96 @@
-use actix_web::http::HeaderMap;
+use actix_service::{Service, Transform};
+use actix_web::{
+    dev::{ServiceRequest, ServiceResponse},
+    web::Data,
+    Error, HttpResponse,
+};
+use futures::{
+    future::{ok, Ready},
+    Future,
+};
 use sqlx::SqlitePool;
-use thiserror::Error;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
 
-use crate::models::users::User
-use crate::database::models;
-use crate::utilities::token_utils;
+use crate::{utilities::token_utils, models::response::ResponseBody, constants};
 
-#[derive(Error, Debug)]
-pub enum AuthenticationError {
-    #[error("An unknown database error occurred")]
-    Sqlx(#[from] sqlx::Error),
-    #[error("Database Error: {0}")]
-    Database(#[from] models::DatabaseError),
-    #[error("Error while parsing JSON: {0}")]
-    SerDe(#[from] serde_json::Error),
-    #[error("Error while communicating to GitHub OAuth2: {0}")]
-    InvalidCredentials,
+pub struct Authentication;
+
+impl<S, B> Transform<S> for Authentication
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = AuthenticationMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(AuthenticationMiddleware { service })
+    }
+}
+pub struct AuthenticationMiddleware<S> {
+    service: S,
 }
 
-pub async fn get_user_from_headers(
-    headers: &HeaderMap,
-    conn: &SqlitePool
-) -> Result<User, AuthenticationError> {
-    let token = headers
-        .get("Authorization")
-        .ok_or(AuthenticationError::InvalidCredentials)?
-        .to_str()
-        .map_err(|_| AuthenticationError::InvalidCredentials)?;
+impl<S, B> Service for AuthenticationMiddleware<S>
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
 
-    get_user_from_token(token, conn).await
-}
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
 
-pub async fn get_user_from_token(
-    access_token: &str,
-    conn: &SqlitePool
-) -> Result<User, AuthenticationError> {
-    if let Ok(token_data) = token_utils::decode_token(access_token.to_string()) {
-        if let Ok(username) = token_utils::verify_token(&token_data, pool) {
+    fn call(&mut self, mut req: ServiceRequest) -> Self::Future {
+        let token = req.headers()
+            .get("Authorization") 
+            .and_then(|h| h.to_str().ok())
+            .and_then(|h| {
+                h.strip_prefix("Bearer ")
+                    .map(|stripped_token | stripped_token.to_owned())
+            })
+            .unwrap_or_else(String::new);
 
-        }
+        let pool = req.app_data::<Data<SqlitePool>>();
+
+        let token_validation_future = async move {
+            let pool = pool
+                .ok_or(actix_web::error::ErrorInternalServerError(
+                    "Missing database pool"
+                ))?;
+            let token_data = token_utils::decode_token(token)
+                .map_err(|_| {
+                    HttpResponse::Unauthorized()
+                        .json(ResponseBody::new(constants::MESSAGE_INVALID_TOKEN, constants::EMPTY))
+                })?;
+
+            if !token.is_empty() && token_utils::verify_token(&token_data, pool).await.is_ok() {
+                let res = self.service.call(req).await?;
+                Ok(res)
+            } else {
+                Err(HttpResponse::Unauthorized()
+                    .json(ResponseBody::new(
+                        constants::MESSAGE_INVALID_TOKEN, 
+                        constants::EMPTY
+                    ))
+                    .into()
+                )
+            }
+        };
+
+        Box::pin(token_validation_future)
     }
 }
