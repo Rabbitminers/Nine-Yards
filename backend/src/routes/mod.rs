e1158b108314e43ae9777c2072bb42180f286d79
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-10 13:15:20 +0100

    Added route configs

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,1 +1,8 @@
 pub mod user_routes;
+
+pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
+    cfg.service(
+        actix_web::web::scope("/api")
+            .configure(user_routes::config),
+    );
+}
