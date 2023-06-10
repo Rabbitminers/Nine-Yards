52ddced12ef712665c00eba43cc7ffbc49894d1f
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-10 13:11:55 +0100

    Implemented database error wrappers

--- backend/src/database/mod.rs
+++ backend/src/database/mod.rs
@@ -1,1 +1,15 @@
+use thiserror::Error;
+
 pub mod sqlite;
+
+#[derive(Error, Debug)]
+pub enum DatabaseError {
+    #[error("Error while interacting with the database: {0}")]
+    Database(#[from] sqlx::error::Error),
+    #[error("Error while trying to generate random ID")]
+    RandomId,
+    #[error("A database request failed")]
+    Other(String),
+    #[error("Error while parsing JSON: {0}")]
+    Json(#[from] serde_json::Error),
+}
