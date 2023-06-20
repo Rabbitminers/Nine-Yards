be884aafe14100f06d99791239575ff86b4d4020
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-20 18:42:18 +0100

    Added custom authentication error types

---
+++ backend/src/utilities/auth_utils.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AuthenticationError {
+    #[error("An unknown database error occurred")]
+    Sqlx(#[from] sqlx::Error),
+
+    #[error("Database error: {0}")]
+    Database(#[from] crate::models::DatabaseError),
+
+    #[error("Error while parsing JSON: {0}")]
+    SerDe(#[from] serde_json::Error),
+
+    #[error("Invalid Authentication Credentials")]
+    InvalidCredentials,
+
+    #[error("You are not authorized to perform this action")]
+    Unauthorized,
+
+    #[error("Invalid or missing token. Please login again")]
+    InvalidToken,
+
+    #[error("You need to be member of this project in order to perform this action")]
+    NotMember,
+}
--- backend/src/utilities/mod.rs
+++ backend/src/utilities/mod.rs
@@ -1,2 +1,3 @@
 pub mod token_utils;
 pub mod validation_utils;
+pub mod auth_utils;
