3e725865b3c975f10c08b5b6975c3e02f8bdf89b
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-20 18:40:44 +0100

    Updated migrations to support sub tasks

--- backend/migrations/20230603155502_init.down.sql
+++ backend/migrations/20230603155502_init.down.sql
@@ -1,6 +1,8 @@
 -- Add down migration script here
 DROP TABLE IF EXISTS users;
 
+DROP TABLE IF EXISTS login_history;
+
 DROP TABLE IF EXISTS project_members;
 
 DROP TABLE IF EXISTS projects;
@@ -8,5 +10,9 @@ DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS task_groups;
 
 DROP TABLE IF EXISTS tasks;
+
+DROP TABLE IF EXISTS lables;
+
+DROP TABLE IF EXISTS sub_tasks
 
 DROP TABLE IF EXISTS notifications;
--- backend/migrations/20230603155502_init.up.sql
+++ backend/migrations/20230603155502_init.up.sql
@@ -26,7 +26,8 @@ CREATE TABLE IF NOT EXISTS projects (
     id TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     owner TEXT REFERENCES users NOT NULL,
-    icon_url TEXT NOT NULL
+    icon_url TEXT NOT NULL,
+    public BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS task_groups (
@@ -43,8 +44,24 @@ CREATE TABLE IF NOT EXISTS tasks (
     name TEXT NOT NULL,
     information TEXT NOT NULL,
     creator TEXT REFERENCES users NOT NULL,
-    assignee TEXT REFERENCES users NOT NULL,
-    position INTEGER NOT NULL
+    due DATETIME,
+    primary_colour TEXT NOT NULL,
+    accent_colour TEXT NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS lables (
+    id TEXT PRIMARY KEY NOT NULL,
+    task_id TEXT REFERENCES tasks NOT NULL,
+    project_member_id TEXT REFERENCES project_members NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sub_tasks (
+    id TEXT PRIMARY KEY NOT NULL,
+    task_id TEXT REFERENCES tasks NOT NULL,
+    assignee TEXT REFERENCES project_members,
+    body TEXT NOT NULL,
+    completed BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS notifications (
