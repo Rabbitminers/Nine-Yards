5eb43d0ee031bb5d95c1cabaa2322b84a2f8c117
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-20 18:42:00 +0100

    Overhauled id's system

--- backend/src/models/ids.rs
+++ backend/src/models/ids.rs
@@ -1,118 +1,149 @@
-use uuid::Uuid;
+use sqlx::Type;
 
+const BASE62_CHARS: [u8; 62] =
+    *b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const ID_RETRY_COUNT: usize = 20; 
 
 macro_rules! id_type {
-    ($vis:vis $function_name:ident, $struct:ty, $id_length:expr, $select_stmnt:literal, $id_function:expr) => {
-        $vis async fn $function_name (
-            conn: &sqlx::SqlitePool,
-        ) -> Result<$struct, super::DatabaseError> {
-            let mut retry_count = 0;
-            let length = $id_length;
+    ($vis:vis, $struct:ty, $id_length:expr, $select_stmnt:literal) => {
+        impl $struct {
+            $vis async fn generate(
+                conn: &sqlx::SqlitePool,
+            ) -> Result<$struct, super::DatabaseError> {
+                let mut retry_count = 0;
+                let length = $id_length;
 
-            let censor = censor::Censor::Standard + censor::Censor::Sex;
-            let mut id;
+                let censor = censor::Censor::Standard + censor::Censor::Sex;
+                let mut id;
 
-            loop {
-                id = Uuid::new_v4().to_simple().to_string();
+                loop {
+                    id = crate::models::ids::generate_base62_id(length);
 
-                let results = sqlx::query!($select_stmnt, id)
-                    .fetch_one(conn)
-                    .await?;
+                    let results = sqlx::query!($select_stmnt, id)
+                        .fetch_one(conn)
+                        .await?;
 
-                let count = results.count;
+                    let count = results.count;
 
-                if !censor.check(&id) && count == 0 {
-                    break;
+                    if !censor.check(&id) && count == 0 {
+                        break;
+                    }
+                    
+                    retry_count += 1;
+                    if retry_count > ID_RETRY_COUNT {
+                        return Err(super::DatabaseError::RandomId);
+                    }
+
                 }
                 
-                retry_count += 1;
-                if retry_count > ID_RETRY_COUNT {
-                    return Err(super::DatabaseError::RandomId);
-                }
-
+                Ok(Self(id))
             }
-            
-            Ok($id_function(id))
         }
     };
 }
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+fn generate_base62_id(length: usize) -> String {
+    let mut id = String::with_capacity(length);
+    let base: u64 = 62;
+
+    let mut num = rand::random::<u64>();
+
+    for _ in 0..length {
+        id.push(BASE62_CHARS[(num % base) as usize] as char);
+        num /= base;
+    }
+
+    id.chars().rev().collect()
+}
+
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct UserId(pub String);
 
 id_type!(
-    pub generate_user_id,
+    pub,
     UserId, 
-    10,
-    "SELECT COUNT(*) as count FROM users WHERE id = ?",
-    UserId
+    8,
+    "SELECT COUNT(*) as count FROM users WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct ProjectMemberId(pub String);
 
 id_type!(
-    pub generate_project_member_id,
+    pub,
     ProjectMemberId, 
     10,
-    "SELECT COUNT(*) as count FROM project_members WHERE id = ?",
-    ProjectMemberId
+    "SELECT COUNT(*) as count FROM project_members WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct ProjectId(pub String);
 
 id_type!(
-    pub generate_project_id,
+    pub,
     ProjectId, 
-    10,
-    "SELECT COUNT(*) as count FROM projects WHERE id = ?",
-    ProjectId
+    8,
+    "SELECT COUNT(*) as count FROM projects WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct TaskGroupId(pub String);
 
 id_type!(
-    pub generate_task_group_id,
+    pub,
     TaskGroupId,
     8,
-    "SELECT COUNT(*) as count FROM task_groups WHERE id = ?",
-    TaskGroupId
+    "SELECT COUNT(*) as count FROM task_groups WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct TaskId(pub String);
 
 id_type!(
-    pub generate_tasks_id,
+    pub,
     TaskId,
     8,
-    "SELECT COUNT(*) as count FROM tasks WHERE id = ?",
-    TaskId
+    "SELECT COUNT(*) as count FROM tasks WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
+pub struct SubTaskId(pub String);
+
+id_type!(
+    pub,
+    SubTaskId,
+    12,
+    "SELECT COUNT(*) as count FROM sub_tasks WHERE id = ?"
+);
+
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct LoginHistoryId(pub String);
 
 id_type!(
-    pub generate_login_history_id,
+    pub,
     LoginHistoryId,
-    8,
-    "SELECT COUNT(*) as count FROM login_history WHERE id = ?",
-    LoginHistoryId
+    10,
+    "SELECT COUNT(*) as count FROM login_history WHERE id = ?"
 );
 
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(transparent)]
 pub struct NotifcationId(pub String);
 
 id_type!(
-    pub generate_notification_id,
+    pub,
     NotifcationId,
-    8,
-    "SELECT COUNT(*) as count FROM notifications WHERE id = ?",
-    NotifcationId
+    12,
+    "SELECT COUNT(*) as count FROM notifications WHERE id = ?"
 );
+
+
 
 
