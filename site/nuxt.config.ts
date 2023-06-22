5d95ac6a6ef94c17bd523994c4c3e4dcf0132f0e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-23 00:14:18 +0100

    Updated nuxt configuration

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nine Yards'
+    }
+  },
   devtools: { 
     enabled: true 
   },
