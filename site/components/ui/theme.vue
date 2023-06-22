4b75a533ad7dd6e61cf7a03851ee91c30f9208f2
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-22 16:32:25 +0100

    Added icon components

---
+++ site/components/ui/theme.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+button(
+    class="mode-switch" 
+    title="Switch Theme"
+    @click="toggle_dark()"
+)
+    icons-moon(:class="{ active: is_dark }")
+</template>
+
+<script setup lang="ts">
+import {toggle_dark, is_dark} from '~/api/dark'
+</script>
