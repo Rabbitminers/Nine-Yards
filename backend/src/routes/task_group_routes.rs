a14a655c4137dfddbadf52fe6796c8f8f8c5e5b8
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-24 23:41:16 +0100

    Updated task group routes to new api layout

--- backend/src/routes/task_group_routes.rs
+++ backend/src/routes/task_group_routes.rs
@@ -1,104 +1,42 @@
-use actix_web::{web, HttpResponse, get, http::StatusCode, post, delete};
+use actix_web::{web, HttpResponse, get, http::StatusCode, post, delete, dev::ServiceRequest};
 use log::info;
 use validator::Validate;
 
-use crate::{database::SqlPool, models::{ids::{TaskGroupId, ProjectId}, tasks::{TaskGroup, TaskGroupBuilder}, projects::{Project, ProjectMember, Permissions}, audit::Audit}, response, utilities::{validation_utils::validation_errors_to_string, auth_utils::AuthenticationError::{NotMember, Unauthorized, MissingPermissions}}};
+use crate::{database::SqlPool, models::{ids::{TaskGroupId, ProjectId}, tasks::{TaskGroup, TaskGroupBuilder, Task, TaskBuilder}, projects::{Project, ProjectMember, Permissions}, audit::Audit}, response, utilities::{validation_utils::validation_errors_to_string, auth_utils::AuthenticationError::{NotMember, Unauthorized, MissingPermissions}}, middleware::project::ProjectAuthentication};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
         .service(
-    web::scope("/task-groups")
-                .service(get_many)
-                .service(create)
+    web::scope("/task-groups/{task_group_id}")
+                .wrap(ProjectAuthentication {
+                    id_key: "task_group_id".to_string(),
+                    table_name: Some("task_groups".to_string())
+                })
+                .service(get)
+                .service(get_tasks)
+                .service(edit)
+                .service(remove)
                 .service(
-                    web::scope("/{task_group_id}")
-                        .service(get)
+            web::scope("/tasks")        
                         .service(get_tasks)
-                        .service(edit)
-                        .service(remove)
+                        .service(create_task)
                 )
         );
 }
 
-#[get("/")]
-pub async fn get_many(
-    path: web::Path<(String,)>,
-    pool: web::Data<SqlPool>
-) -> Result<HttpResponse, super::ApiError> {
-    let project_id = ProjectId(path.0.clone());
-
-    let mut transaction = pool.begin().await?;
-
-    let groups = Project::get_task_groups(project_id, &mut transaction).await?;
-
-    transaction.commit().await?;
-    
-    response!(StatusCode::OK, groups, "Successfully retrieved task groups")
-}
-
-#[post("/create")]
-pub async fn create(
-    form: web::Json<TaskGroupBuilder>,
-    path: web::Path<(String,)>,
-    req: actix_web::HttpRequest,
-    pool: web::Data<SqlPool>
-) -> Result<HttpResponse, super::ApiError> {
-    let mut transaction = pool.begin().await?;
-    let form = form.into_inner();
-
-    let project_id = ProjectId(path.0.clone());
-
-    let member = ProjectMember::from_request(req, &mut transaction).await?
-        .ok_or_else(|| super::ApiError::Unauthorized(NotMember))?;
-
-    if !member.permissions.contains(Permissions::MANAGE_TASKS) {
-        return Err(super::ApiError::Unauthorized(MissingPermissions));
-    }
-
-    form.validate().map_err(|err| 
-            super::ApiError::Validation(validation_errors_to_string(err, None)))?;
-
-    let task_group = form.create(project_id, &mut transaction).await?;
-
-    Audit::create(&member, format!("Created new task group '{}'", task_group.name), &mut transaction).await?;
-    transaction.commit().await?;
-
-    response!(StatusCode::CREATED, task_group, "Successfully created task group")
-}
+// Id-ed Task group routes
 
 #[get("/")]
 pub async fn get(
-    path: web::Path<(String, String)>,
+    path: web::Path<(String,)>,
     pool: web::Data<SqlPool>
 ) -> Result<HttpResponse, super::ApiError> {
-    let project_id = ProjectId(path.0.clone());
-    let group_id = TaskGroupId(path.1.clone());
+    let group_id = TaskGroupId(path.0.clone());
 
-    let mut transaction = pool.begin().await?;
+    let task_group = TaskGroup::get(group_id, &**pool).await?
+        .ok_or_else(|| super::ApiError::NotFound("Task group not found".to_string()))?;
 
-    if let Some(group) = TaskGroup::get(group_id.clone(), project_id, &mut transaction).await? {        
-        transaction.commit().await?;
-
-        info!("Retrieved task group: {}", group.id.0);
-        response!(StatusCode::OK, group, "Successfully retrieved task group")
-    } else {
-        info!("Failed to retrieve task group: {}", group_id.0);
-        Err(super::ApiError::NotFound("Could not find task group".to_string()))
-    }
-}
-
-#[get("/tasks")]
-pub async fn get_tasks(
-    path: web::Path<(String, String)>,
-    pool: web::Data<SqlPool>,
-) -> Result<HttpResponse, super::ApiError> {
-    let group_id = TaskGroupId(path.1.clone());
-    let project_id = ProjectId(path.0.clone());
-    
-    let tasks = TaskGroup::get_tasks_full(project_id,group_id, &pool).await?;
-
-    info!("Retrieved {} tasks", tasks.len());
-    response!(StatusCode::OK, tasks, "Successfully retrieved tasks")
+    response!(StatusCode::OK, task_group, "Successfully retrieved task group")
 }
 
 
@@ -111,18 +49,18 @@ pub struct TaskGroupEdit {
 
 #[post("/edit")]
 pub async fn edit(
-    path: web::Path<(String, String)>,
+    path: web::Path<(String,)>,
     pool: web::Data<SqlPool>,
     form: web::Json<TaskGroupEdit>,
     req: actix_web::HttpRequest,
 ) -> Result<HttpResponse, super::ApiError> {
     let form = form.into_inner();
+    let project_id = super::project_id(&req)?;
 
     form.validate().map_err(|err| 
         super::ApiError::Validation(validation_errors_to_string(err, None)))?;
-
-    let project_id = ProjectId(path.0.clone());
-    let group_id = TaskGroupId(path.1.clone());
+        
+    let group_id = TaskGroupId(path.0.clone());
 
     let mut transaction = pool.begin().await?;
 
@@ -133,7 +71,7 @@ pub async fn edit(
         return Err(super::ApiError::Unauthorized(MissingPermissions));
     }
 
-    let group = TaskGroup::get(group_id.clone(), project_id.clone(), &mut transaction).await?
+    let group = TaskGroup::get(group_id.clone(), &mut transaction).await?
         .ok_or_else(|| super::ApiError::NotFound("Could not find task group to edit".to_string()))?;
 
     if let Some(name) = form.name {
@@ -159,6 +97,21 @@ pub async fn edit(
     }
 
     if let Some(position) = form.position {
+        // Move existing groups out of the ways
+        sqlx::query!(
+            "
+            UPDATE task_groups
+            SET position = position + 1
+            WHERE position >= $1
+            AND project_id = $2
+            ",
+            position,
+            project_id
+        )
+        .execute(&mut transaction)
+        .await?;
+
+        // Insert into now empty slot
         sqlx::query!(
             "
             UPDATE task_groups
@@ -187,14 +140,13 @@ pub async fn edit(
 
 #[delete("/")]
 pub async fn remove(
-    path: web::Path<(String, String)>,
+    path: web::Path<(String,)>,
     req: actix_web::HttpRequest,
     pool: web::Data<SqlPool>
 ) -> Result<HttpResponse, super::ApiError> {
     let mut transaction = pool.begin().await?;
 
-    let project_id = ProjectId(path.0.clone());
-    let group_id = TaskGroupId(path.1.clone());
+    let group_id = TaskGroupId(path.0.clone());
 
     let member = ProjectMember::from_request(req, &mut transaction).await?
         .ok_or_else(|| super::ApiError::Unauthorized(NotMember))?;
@@ -203,13 +155,51 @@ pub async fn remove(
         return Err(super::ApiError::Unauthorized(MissingPermissions));
     }
 
-    let group = TaskGroup::get(group_id.clone(), project_id.clone(), &mut transaction).await?
+    let group = TaskGroup::get(group_id.clone(), &mut transaction).await?
         .ok_or_else(|| super::ApiError::NotFound("Could not find task group to remove".to_string()))?;
     
-    TaskGroup::remove(project_id, group_id, &mut transaction).await?;
+    TaskGroup::remove(group_id, &mut transaction).await?;
     Audit::create(&member, format!("Removed task group '{}'", group.name), &mut transaction).await?;
 
     transaction.commit().await?;
 
     response!(StatusCode::OK, "Successfully removed task group")
+}
+
+// Generic Task routes
+
+#[post("/create")]
+pub async fn create_task(
+    path: web::Path<(String,)>,
+    req: actix_web::HttpRequest,
+    pool: web::Data<SqlPool>,
+    form: web::Json<TaskBuilder>,
+) -> Result<HttpResponse, super::ApiError> {
+    let mut transaction = pool.begin().await?;
+    let form = form.into_inner();
+
+    let project_id = super::project_id(&req)?;
+
+    let member = ProjectMember::from_request(req, &mut transaction).await?
+        .ok_or_else(|| super::ApiError::Unauthorized(NotMember))?;
+
+    if !member.permissions.contains(Permissions::MANAGE_TASKS) {
+        return Err(super::ApiError::Unauthorized(MissingPermissions));
+    }
+
+    form.validate().map_err(|err| 
+            super::ApiError::Validation(validation_errors_to_string(err, None)))?;
+    let task = form.create(project_id, &mut transaction).await?;
+
+    response!(StatusCode::OK, task, "Successfully created task")
+}
+
+#[get("/")]
+pub async fn get_tasks(
+    path: web::Path<(String,)>,
+    pool: web::Data<SqlPool>,
+) -> Result<HttpResponse, super::ApiError> {
+    let group_id = TaskGroupId(path.0.clone());
+    let tasks = TaskGroup::get_tasks_full(group_id, &pool).await?;
+    response!(StatusCode::OK, tasks, "Successfully retrieved tasks")
 }
