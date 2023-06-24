535119f60f1b9ff9ed9357c7e476f3beb62eb31f
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-24 23:38:31 +0100

    Extracted token from headers to common module functionality

--- backend/src/middleware/mod.rs
+++ backend/src/middleware/mod.rs
@@ -1,2 +1,14 @@
+use actix_web::http::header::HeaderMap;
+
 pub mod auth;
 pub mod project;
+
+pub fn get_token_from_headers(headers: &HeaderMap) -> String {
+    headers.get("Authorization") 
+        .and_then(|h| h.to_str().ok())
+        .and_then(|h| {
+            h.strip_prefix("Bearer ")
+                .map(|stripped_token | stripped_token.to_owned())
+        })
+        .unwrap_or_else(String::new)
+}
