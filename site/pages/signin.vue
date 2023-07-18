6038bc2eeffb3c9fb833fa9641d992a43d181a84
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-18 21:52:42 +0100

    Implemented signup & signin pages

---
+++ site/pages/signin.vue
@@ -0,0 +1,105 @@
+<template lang="pug">
+form.projects-section.login-form
+  section.login-form-inputs.columns
+    header.projects-section-header
+        p Welcome Back!
+  
+    label.input-bar.rows
+      icons-user.input-icon
+      input(
+        id="username"
+        type="text"
+        placeholder="Username or Email"
+      )
+    label.input-bar.rows
+      icons-password.input-icon
+      input(
+        id="password"
+        type="password"
+        placeholder="Password"
+      )
+    label.input-bar.rows
+      input.login-form-submit(
+        type="submit" 
+        value="Sign In!"
+      )
+    router-link(to="/signup")
+      a Dont have an account?
+  icons-sleeping.login-form-graphic(
+    :shadowed="true"
+  )
+</template>
+
+<script lang="ts">
+definePageMeta({
+  layout: "default",
+});
+
+export default {
+  name: "login",
+}
+</script>
+
+<style lang="scss">
+$theme: var(--colour-green);
+
+.login-form {
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+
+  &-inputs {
+    width: 100%;
+    margin: auto;
+    align-items: center;
+    gap: inherit;
+  }
+
+  span {
+    font-size: large;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-secondary);
+  }
+}
+
+.sub-card {
+  background-color: $theme;
+  padding: 1.5em;
+  border-radius: 32px;
+}
+
+.input-bar {
+  @extend .sub-card;
+  gap: 0.5em;
+
+  > input {
+    background: transparent;
+    border: none;
+    width: 100%;
+    color: var(--color-secondary);
+
+    &:focus {
+      outline-color: none;
+    }
+  }
+}
+
+@media screen and (max-width: 540px) {
+  .login-form {
+    flex-direction: column;
+  }
+}
+
+
+.fern-wool {
+  fill: $theme;
+
+  &-shadow {
+    fill: #fff;
+    opacity: 0.53;
+  }
+}
+</style>
