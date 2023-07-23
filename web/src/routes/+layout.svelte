3b7f554abd84cc77e8d526f12cbf8dc0eae5c733
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-23 23:05:28 +0100

    Implemented global layout

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '../app.css'
+    
+	import Header from '../components/layout/header.svelte';
+</script>
+
+<html lang="html" class="max-w-7xl max-h-screen m-auto" data-theme="nineyardslight">
+
+<Header/>
+
+<main class="p-2">
+    <slot />
+</main>
+
+</html>
