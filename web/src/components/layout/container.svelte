12d48a38d2ec5935d7c31529d50e556417c79ea6
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-23 23:03:44 +0100

    Implemented custom container layout component

---
+++ web/src/components/layout/container.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title: string = "";
+</script>
+
+<article class="card w-full h-full grow-1 bg-neutral shadow-xl">
+    <span class="card-body">
+        {#if title}
+            <section class="card-title w-full">
+                <h1>{title}</h1>
+            </section>
+        {/if}
+        <slot/>
+    </span>
+</article>
