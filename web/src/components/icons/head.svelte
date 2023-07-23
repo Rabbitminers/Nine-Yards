8a781e9971d0167aa1116f5166e0223edf1d5c51
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-23 23:03:26 +0100

    Implemented updated fern components

---
+++ web/src/components/icons/head.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let wool: string = "fill-neutral";
+    export let shadow: string = "fill-base-100";
+</script>
+
+<svg width="100%" height="100%" viewBox="0 0 2481 2481" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" 
+    style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g>
+        <ellipse cx="1468.39" cy="752.258" rx="778.198" ry="752.258" class="fill-accent" />
+        <ellipse cx="713.077" cy="752.258" rx="457.763" ry="442.505" class="fill-accent" />
+        <ellipse cx="1903.26" cy="1283.26" rx="572.204" ry="553.131" class="fill-accent" />
+        <ellipse cx="1937.59" cy="1861.34" rx="354.767" ry="350.724" class="fill-accent" />
+        <ellipse cx="1925.31" cy="1849.98" rx="229.726" ry="224.917" style="fill:#ffd5ac;" />
+        <ellipse cx="713.077" cy="752.258" rx="343.323" ry="331.879" class={wool} />
+        <ellipse cx="1903.26" cy="1283.26" rx="457.763" ry="442.505" class={shadow} />
+        <ellipse cx="1834.6" cy="1128.39" rx="389.099" ry="376.129" class={wool} />
+        <ellipse cx="1172.94" cy="1011.82" rx="457.763" ry="442.505" class="fill-accent" />
+        <ellipse cx="884.738" cy="1747.89" rx="881.195" ry="732.422" class="fill-accent" />
+        <ellipse cx="982.526" cy="1604.67" rx="171.661" ry="142.68" class={wool} />
+        <ellipse cx="982.526" cy="1506.45" rx="194.549" ry="161.703" class="fill-accent" />
+        <ellipse cx="1468.39" cy="752.258" rx="663.757" ry="641.632" class={shadow} />
+        <ellipse cx="1388.28" cy="712.204" rx="583.648" ry="571.06" class={wool} />
+        <path
+            d="M320.546,1703.42c25.878,-41.405 77.633,-41.405 103.511,-20.703c25.879,20.703 25.879,62.107 0,103.511c-18.115,31.054 -64.694,62.107 -103.511,82.809c-38.817,-20.702 -85.397,-51.755 -103.511,-82.809c-25.878,-41.404 -25.878,-82.808 -0,-103.511c25.878,-20.702 77.633,-20.702 103.511,20.703Z"
+            class={wool} />
+    </g>
+</svg>
