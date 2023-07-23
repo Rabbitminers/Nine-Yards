48f020136c0f6f14e5502ca4d2d580c16e56e9ac
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-23 23:06:21 +0100

    Re-added basic about project page

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Sleeping from "../components/icons/sleeping.svelte";
+
+</script>
+
+<div class="hero">
+    <div class="hero-content flex-col lg:flex-row">
+        <div class="max-w-sm">
+            <Sleeping/>
+        </div>
+        <div>
+        <h1 class="text-5xl font-bold">Nine Yards</h1>
+        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
---
+++ web/src/routes/projects/[id]/about/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import Container from "../../../../components/layout/container.svelte";
+import Task from "../../../../components/task/task.svelte";
+	import Avatar from "../../../../components/user/avatar.svelte";
+import type { Task as TaskData } from "../../../../types/tasks";
+
+const data: TaskData = {
+	id: "123",
+	project_id: "456",
+	task_group_id: "789",
+	name: "This is a task!",
+	creator: "Rabbitminers",
+    position: 1,
+    due: 1689807600000 + (86400 * 1000 * 3),
+	primary_colour: "#ffffff",
+	accent_colour: "#ffffff",
+	created: 1689807600000 - (86400 * 1000 * 3)
+} 
+
+</script>
+
+<main class="flex flex-row">
+<Container title="About">
+    <section class="grid grid-cols-2 gap">
+        <Task {data} viewType='grid'/>
+    </section>
+</Container>
+
+<Container title="Recent Updates">
+
+</Container>
+</main>
