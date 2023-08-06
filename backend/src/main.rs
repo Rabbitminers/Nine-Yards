9c0af44182e6246294f903ccc3d8b6031151ffbd
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-08-06 23:58:19 +0100

    Added swagger ui

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,12 @@
+#![forbid(unsafe_code)]
+
 extern crate sqlx;
 #[macro_use]
 extern crate serde;
 
-use axum::Router;
+use actix_web::{web, App};
 use database::SqlPool;
+use utoipa_swagger_ui::SwaggerUi;
 
 pub mod database;
 pub mod models;
@@ -13,18 +16,34 @@ pub mod api;
 pub mod services;
 pub mod error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(utoipa::OpenApi)]
+#[openapi(paths())]
+pub struct ApiDoc;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     dotenv::dotenv().expect("Failed to read .env file");
 
-    let pool: SqlPool = database::connect().await?;
-    
-    let app = axum::Router::new()
-        .layer(pool);
-    
-    axum::Server::bind(&"0.0.0.0".parse().unwrap())
-        .serve(app)
-        .await?;
+    let sql_pool: SqlPool = database::sql::connect().await?; 
 
-    Ok(())
+    actix_web::HttpServer::new(move || {
+        App::new()
+            .wrap(actix_cors::Cors::default()
+                .allow_any_origin()
+                .allow_any_header()
+                .allow_any_method()
+                .max_age(3600)
+                .send_wildcard()
+            )
+            .wrap(actix_web::middleware::Compress::default())
+            .app_data(web::Data::new(sql_pool.clone()))
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-docs/openapi.json", ApiDoc::openapi())
+            )   
+            .configure(api::v1::config)
+    })
+    .bind("127.0.0.1:8000")
+    .run()
+    .await
 }
