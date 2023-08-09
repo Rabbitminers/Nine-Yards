8e193d06c2a3d00cd5dc9f8aba1fae1e2342408b
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-08-09 19:22:51 +0100

    Made CORS policy more strict

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,9 @@ extern crate sqlx;
 #[macro_use]
 extern crate serde;
 
-use axum::Router;
+use std::{time::Duration, net::SocketAddr};
+
+use axum::{Router, http::{Method, header}, extract::{FromRef, State}};
 use database::SqlPool;
 use tower_http::{cors::{CorsLayer, Any}, trace::TraceLayer};
 
@@ -13,29 +15,46 @@ pub mod models;
 pub mod response;
 pub mod middleware;
 pub mod api;
-pub mod services;
 pub mod error;
 
 #[derive(utoipa::OpenApi)]
 #[openapi(paths())]
 pub struct ApiDoc;
 
+#[derive(Clone)]
+pub struct AppState {
+    pub pool: SqlPool,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().expect("Failed to read .env file");
 
-    let sql_pool: SqlPool = database::sql::connect().await?; 
+    let pool: SqlPool = database::sql::connect().await?; 
 
     let app = Router::new()
         .layer(CorsLayer::default()
+            .allow_headers([
+                header::AUTHORIZATION,
+                header::WWW_AUTHENTICATE,
+                header::CONTENT_TYPE,
+                header::ORIGIN
+            ])
+            .allow_methods([
+                Method::GET,
+                Method::PUT,
+                Method::POST,
+                Method::DELETE,
+            ])
             .allow_origin(Any)
-            .allow_headers(Any)
-            .allow_methods(Any)
+            .max_age(Duration::from_secs(86400))
         )
         .layer(TraceLayer::new_for_http())
-        .with_state(sql_pool);
+        .with_state(AppState { pool });
 
-    axum::Server::bind(dotenv::var("BIND_ADDR").unwrap())
+    let socket_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&socket_addr)
         .serve(app.into_make_service())
         .await?;
 
