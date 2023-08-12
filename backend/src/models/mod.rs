7530dfb7be4ecb1a77455daf038b65d5758cf5e2
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-08-12 23:13:51 +0100

    Removed legacy database errors from model module

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,27 +1,6 @@
-use thiserror::Error;
-
 pub mod users;
 pub mod id;
 pub mod projects;
 pub mod tasks;
-
-#[derive(Error, Debug)]
-pub enum DatabaseError {
-    #[error("Error while interacting with the database: {0}")]
-    Database(#[from] sqlx::error::Error),
-
-    #[error("Error while trying to generate random ID")]
-    RandomId,
-
-    #[error("A database request failed")]
-    Other(String),
-
-    #[error("Error while parsing JSON: {0}")]
-    Json(#[from] serde_json::Error),
-
-    #[error("Entry already exists")]
-    AlreadyExists,
-
-    #[error("{0} not found")]
-    NotFound(String)
-}
+pub mod tokens;
+pub mod audits;
