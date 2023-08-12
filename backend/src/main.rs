3ebc8c732d24b694899adabd5f5740c5911ec92f
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-08-12 23:18:14 +0100

    Added swagger ui layer to router

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,9 +6,15 @@ extern crate serde;
 
 use std::{time::Duration, net::SocketAddr};
 
-use axum::{Router, http::{Method, header}, extract::{FromRef, State}};
+use axum::Router;
+use axum::http::{Method, header};
+use axum::extract::FromRef;
 use database::SqlPool;
-use tower_http::{cors::{CorsLayer, Any}, trace::TraceLayer};
+use tower_http::add_extension::AddExtensionLayer;
+use tower_http::cors::{CorsLayer, Any};
+use tower_http::trace::TraceLayer;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 pub mod database;
 pub mod models;
@@ -17,11 +23,11 @@ pub mod middleware;
 pub mod api;
 pub mod error;
 
-#[derive(utoipa::OpenApi)]
+#[derive(OpenApi)]
 #[openapi(paths())]
 pub struct ApiDoc;
 
-#[derive(Clone)]
+#[derive(Clone, FromRef)]
 pub struct AppState {
     pub pool: SqlPool,
 }
@@ -49,9 +55,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .allow_origin(Any)
             .max_age(Duration::from_secs(86400))
         )
+        .layer(AddExtensionLayer::new(pool))
         .layer(TraceLayer::new_for_http())
-        .with_state(AppState { pool });
-
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()));
+        
     let socket_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     axum::Server::bind(&socket_addr)
