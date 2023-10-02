9f51247210b6985c02cb347588c4b4a54edbd85a
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-10-02 16:10:55 +0100

    Registered all endpoints and schemas for api docs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,8 +11,8 @@ use axum::http::{Method, header};
 use database::SqlPool;
 use tower_http::cors::{CorsLayer, Any};
 use tower_http::trace::TraceLayer;
+use tracing::info;
 use utoipa::OpenApi;
-use utoipa::openapi::info;
 use utoipa_swagger_ui::SwaggerUi;
 
 pub mod database;
@@ -30,6 +30,11 @@ pub mod utilities;
         version = "0.0.1",
     ),
     paths(
+        api::v1::users::get_current_user,
+        api::v1::users::get_user_by_id,
+        api::v1::users::register,
+        api::v1::users::login,
+
         api::v1::projects::create_project,
         api::v1::projects::get_memberships_from_user,
         api::v1::projects::get_project_by_id,
@@ -39,7 +44,22 @@ pub mod utilities;
         api::v1::projects::get_members,
         api::v1::projects::invite_member,
         api::v1::projects::get_task_groups,
-        api::v1::projects::create_task_group
+        api::v1::projects::create_task_group,
+
+        api::v1::task_groups::get_task_group_by_id,
+        api::v1::task_groups::edit_task_group,
+        api::v1::task_groups::remove_task_group,
+        api::v1::task_groups::get_tasks,
+
+        api::v1::tasks::get_task,
+        api::v1::tasks::edit_task,
+        api::v1::tasks::remove_task,
+        api::v1::tasks::get_sub_tasks,
+        api::v1::tasks::create_sub_task,
+
+        api::v1::sub_tasks::get_sub_task_by_id,
+        api::v1::sub_tasks::edit_sub_task,
+        api::v1::sub_tasks::remove_sub_task
     ),
     components(schemas(
         models::id::UserId,
@@ -51,11 +71,36 @@ pub mod utilities;
         models::id::AuditId,
         models::id::NotificationId,
         models::id::NotificationActionId,
+
+        models::users::User,
+        models::users::Register,
+        models::users::Login,
+
+        models::audits::Audit,
+
+        models::tokens::Token,
+
+        models::notifications::Notification,
+        models::notifications::NotificationAction,
+        models::notifications::FullNotification,
+        models::notifications::Actions,
+
         models::projects::Project,
         models::projects::EditProject,
         models::projects::ProjectBuilder,
-        models::projects::Permissions,
         models::projects::ProjectMember,
+        
+        models::tasks::TaskGroup,
+        models::tasks::EditTaskGroup,
+        models::tasks::TaskGroupBuilder,
+        models::tasks::Task,
+        models::tasks::SubTasks,
+        models::tasks::FullTask,
+        models::tasks::TaskBuilder,
+        models::tasks::EditTask,
+        models::tasks::SubTask,
+        models::tasks::EditSubTask,
+        models::tasks::SubTaskBuilder,
     ))
 )]
 pub struct ApiDoc;
@@ -95,7 +140,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .with_state(ApiContext { pool });
 
-    axum::Server::bind(&"0.0.0.0:3000".parse()?)
+    let address = dotenv::var("BIND_ADDR").expect("Missing address");
+
+    info!("Starting Nine Yards server on http://{}, view the docs at http://{}/swagger-ui", address, address);
+
+    axum::Server::bind(&address.parse()?)
         .serve(app.into_make_service())
         .await?;
 
