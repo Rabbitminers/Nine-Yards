e2bd532a9e029dd23b161b9001dc315be16edb34
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-10-03 11:07:25 +0100

    Generated client library from api spec

---
+++ client/models/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Audit';
+export * from './AuthenticatedUser';
+export * from './EditProject';
+export * from './EditSubTask';
+export * from './EditTask';
+export * from './EditTaskGroup';
+export * from './FullNotification';
+export * from './FullTask';
+export * from './Login';
+export * from './Notification';
+export * from './NotificationAction';
+export * from './Project';
+export * from './ProjectBuilder';
+export * from './ProjectMember';
+export * from './Register';
+export * from './SubTask';
+export * from './SubTaskBuilder';
+export * from './Task';
+export * from './TaskBuilder';
+export * from './TaskGroup';
+export * from './TaskGroupBuilder';
+export * from './User';
